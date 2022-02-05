ad1416efcebad5d6265a23d85b74a053335c3d6c
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-02-05 03:31:41 +0100

    Add script to format meta

---
+++ format_meta.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+from pathlib import Path
+
+from numpy.random import SeedSequence
+
+from .aggregate_datasets import load_single_dataset
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # Load
+    parser.add_argument(
+        "--dataset_ratios_path",
+        type=str,
+        required=True,
+        help="path to JSON file containing input dataset ratios. Values ares dictionary: {'dataset_path': str, 'is_catalogue': bool, 'ratio': float}",
+    )
+    parser.add_argument("--split", type=str, default="train", help="split name, default 'train'")
+    parser.add_argument(
+        "--num_proc", type=int, default=1, help="number of procs to use for loading datasets, default 1"
+    )
+    # Parse args
+    args = parser.parse_args()
+    # Post-process args
+    args.dataset_ratios_path = Path(args.dataset_ratios_path)
+    args.save_path = Path(args.save_path)
+    return args
+
+def main():
+    args = get_args()
+
+    with open(args.dataset.ratios_path, "f") as fi:
+        ratios = json.load(fi)
+    seed = SeedSequence(42)
+    seed = seed.spawn(len(ratios))
+
+    ds_ratio = ratios[args.dataset_index]
+    load_single_dataset((ds_ratio, args.split, seed, args.num_proc))
+
+if __name__ == "__main__":
+    main()
