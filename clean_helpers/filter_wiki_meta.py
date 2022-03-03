451542c157e2a1cbaab4d2fa2061f4e79d2e8f6a
SaulLu <55560583+SaulLu@users.noreply.github.com>
2022-03-03 13:25:28 +0100

    Update clean_helpers/filter_wiki_meta.py
    Co-authored-by: Leandro von Werra <lvwerra@users.noreply.github.com>

--- clean_helpers/filter_wiki_meta.py
+++ clean_helpers/filter_wiki_meta.py
@@ -1,2 +1,2 @@
 def filter_wiki_non_text_type(examples):
-    return [True if eval(meta)["type"] == "text" else False for meta in examples["meta"]]
+    return [eval(meta)["type"] == "text" for meta in examples["meta"]]
