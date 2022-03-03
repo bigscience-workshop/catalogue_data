8c8ed00b4612201debec76e76819082d10860a06
Hugo Laurençon <44556846+HugoLaurencon@users.noreply.github.com>
2022-03-03 15:19:52 +0100

    Update filter_small_docs_in_datasets.py

--- clean_helpers/filter_small_docs_in_datasets.py
+++ clean_helpers/filter_small_docs_in_datasets.py
@@ -3,8 +3,3 @@ def build_small_docs_filter(min_word):
         """Discard documents with less than 15 words"""
         return [len(text.split(" ")) >= min_word for text in examples["text"]]
     return filter_small_docs
-
-# in clean.py
-FILTERS = {
-   "filter_en_small_docs": filter_small_docs(15),
-}
