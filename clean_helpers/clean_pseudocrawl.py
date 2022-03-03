f1dd46f5a7e2d2b6f972cc2e5348c305b31e335a
Leandro von Werra <lvwerra@users.noreply.github.com>
2022-03-03 23:26:54 +0100

    Update clean_helpers/clean_pseudocrawl.py
    Co-authored-by: Thomas Wang <24695242+thomasw21@users.noreply.github.com>

--- clean_helpers/clean_pseudocrawl.py
+++ clean_helpers/clean_pseudocrawl.py
@@ -1,11 +1,6 @@
 def remove_lines_with_curly_brackets(examples):
     """Removes lines containing a '{' from the texts."""
-    fixed_texts = []
-    for text in examples["text"]:
-        fixed_lines = []
-        for line in text.split("\n"):
-            if "{" not in line:
-                fixed_lines.append(line)
-        fixed_texts.append("\n".join(fixed_lines))
-    examples["text"] = fixed_texts
-    return examples
+    return {
+        **examples,
+        "text": ["\n".join([line for line in text.split("\n") if "{" not in line]) for text in example["text"]]
+    }
