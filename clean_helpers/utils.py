80aeba1881a9616c38791cfd88e419c330cd0009
Thomas Wang <24695242+thomasw21@users.noreply.github.com>
2022-03-07 15:40:38 +0100

    Add script to generate the columns for deduplication and short filter document (#44)
    * Add script to generate the columns I want
    
    * Factorise language detection code
    
    * Use .apply

--- clean_helpers/utils.py
+++ clean_helpers/utils.py
@@ -1,3 +1,4 @@
+import re
 from typing import Dict
 
 
@@ -5,3 +6,17 @@ def parse_meta(meta) -> Dict:
     if isinstance(meta, str):
         meta = eval(meta)
     return meta
+
+language_regex = re.compile(r"^(?:/gpfswork/rech/six/uty16tp/dataset/tokenization/)?bigscience-catalogue-lm-data/lm_([^_]+)_.*(?:/data)?$")
+def get_language(dataset_name: str):
+    lang_candidate = language_regex.match(dataset_name).group(1)
+
+    # Normalise chinese languages, so that we only consider simplified and traditional chinese as the two chinese languages
+    if lang_candidate in ["zh", "zhs", "zh-cn"]:
+        lang_candidate = "zhs"
+    elif lang_candidate in ["zht", "zh-tw"]:
+        lang_candidate = "zht"
+    else:
+        assert lang_candidate[:2] != "zh"
+
+    return lang_candidate
