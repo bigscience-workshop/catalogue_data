c681cceee07e949f3a8f77c6343172ac87684ec4
Thomas Wang <24695242+thomasw21@users.noreply.github.com>
2022-03-07 15:48:37 +0100

    Remove unecessary deduplication (#45)
    * Remove unecessary deduplication
    
    * blacklist pseudocrawl from dedup_soft

--- download_scripts/change_dataset_preprocessing.py
+++ download_scripts/change_dataset_preprocessing.py
@@ -11,9 +11,9 @@ def get_dedup_args(row: Dict) -> List[str]:
     if "pseudocrawl" in ds_name:
         list_of_dedups = ["dedup_document_on_url", "dedup_document", "dedup_pseudocrawl_newspapers"]
     else:
-        list_of_dedups = ["dedup_document", "dedup_template_soft"]
+        list_of_dedups = ["dedup_document"]
 
-    if all(black_list_ds not in ds_name for black_list_ds in ["open_subtitles", "europarl", "uncorpus"]):
+    if all(black_list_ds not in ds_name for black_list_ds in ["open_subtitles", "europarl", "uncorpus", "pseudocrawl"]):
         list_of_dedups += ["dedup_template_soft"]
 
     list_of_dedups += ["filter_remove_empty_docs"]
