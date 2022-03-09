6ad91afefdbe8aa95bed1bd73f9f4092a9ea815f
Thomas Wang <24695242+thomasw21@users.noreply.github.com>
2022-03-09 23:00:06 +0100

    Making sure that things are sorted (#60)

--- clean_helpers/concatenation.py
+++ clean_helpers/concatenation.py
@@ -1,19 +1,24 @@
+from itertools import groupby
+
 from datasets import Dataset
 
+from clean_helpers.utils import parse_meta
+
+
 def concatenate_lm_fr_ester(ds: Dataset, num_proc: int, batch_size: int) -> Dataset:
-    ids = set([meta["id"].split("_id_")[0] for meta in ds["meta"]])
-    
+    dataset_in_memory = [
+        (*parse_meta(row["meta"])["id"].split("_id_"), row["text"]) for row in ds
+    ]
+    dataset_in_memory.sort()
     new_texts = []
-    new_meta = []
-    for id_source in ids:
-        ds_tmp = ds.filter(
-            lambda exs: [meta["id"].startswith(f"{id_source}_id_") for meta in exs["meta"]], 
-            batched=True, 
-            num_proc=num_proc, 
-            batch_size=batch_size
+    new_metas = []
+    for doc_id, segments in groupby(dataset_in_memory, key=lambda x: x[0]):
+        sorted_segment = sorted(
+            [elt[1:] for elt in segments],
+            key=lambda x: int(x[0])
         )
-        new_texts.append("\n".join(ds_tmp["text"]))
-        new_meta.append({"id":id_source})
+        new_texts.append("\n".join([elt[1] for elt in sorted_segment]))
+        new_metas.append({"id": doc_id})
     
-    new_ds = Dataset.from_dict({"text": new_texts, "meta": new_meta})
+    new_ds = Dataset.from_dict({"text": new_texts, "meta": new_metas})
     return new_ds
