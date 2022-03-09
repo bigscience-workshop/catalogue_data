30bc5414f7d6d88ba1597299741831af7633e76e
SaulLu <lucilesaul.com@gmail.com>
2022-03-09 18:06:41 +0000

    add now proc and batch size

--- clean_helpers/concatenation.py
+++ clean_helpers/concatenation.py
@@ -6,7 +6,12 @@ def concatenate_lm_fr_ester(ds: Dataset, num_proc: int, batch_size: int) -> Data
     new_texts = []
     new_meta = []
     for id_source in ids:
-        ds_tmp = ds.filter(lambda exs: [id_source == meta["id"] for meta in exs["meta"]], batched=True)
+        ds_tmp = ds.filter(
+            lambda exs: [id_source == meta["id"] for meta in exs["meta"]], 
+            batched=True, 
+            num_proc=num_proc, 
+            batch_size=batch_size
+        )
         new_texts.append("\n".join(ds_tmp["text"]))
         new_meta.append({"id":id_source})
     
