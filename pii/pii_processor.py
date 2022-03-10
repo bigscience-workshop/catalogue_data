7a4f719c05583c1398617835a69eabf045c6faac
meg-huggingface <90473723+meg-huggingface@users.noreply.github.com>
2022-03-10 15:37:02 -0800

    Updating pii_processor to handle both the redacted text AND the metadata.

--- pii/pii_processor.py
+++ pii/pii_processor.py
@@ -3,8 +3,10 @@ import json
 from functools import partial
 from bigscience_pii_detect_redact import run_pii
 
-path_jsonl = "/home/lucile/data_pii/pre_processing/lm_eu_oscar.jsonl"
-save_path = "/home/lucile/data_pii/post_processing/lm_eu_oscar.jsonl"
+path_jsonl = "lm_eu_oscar.jsonl"
+save_path = "lm_eu_oscar-redacted.jsonl"
+metadata_save_path = "lm_eu_oscar-redacted-metadata.jsonl"
+
 lang = path_jsonl.split("/")[-1].replace("indic-", "").replace("lm_", "")[:2]
 
 with open(path_jsonl, "r") as fi:
@@ -12,9 +14,14 @@ with open(path_jsonl, "r") as fi:
 
 def func_pii_multiprocessing(line, lang, save_path):
     line = json.loads(line)
-    line["text"] = run_pii(line["text"], lang)
+
+    match_set = run_pii(line["text"], lang)
+    redacted_str, metadata = match_set
+    line["text"] = redacted_str
     with open(save_path, "a") as fi:
         fi.write(f"{json.dumps(line)}\n")
+    with open(metadata_save_path, "a") as fi:
+        fi.write(f"{json.dumps(metadata)}\n")
 
 p = multiprocessing.Pool(processes=multiprocessing.cpu_count()-1)
 async_result = p.map_async(partial(func_pii_multiprocessing, lang=lang, save_path=save_path), jsonlines)
