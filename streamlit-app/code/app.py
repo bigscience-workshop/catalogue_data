93bf6112c70221cd6fd0b887ff6de0c152fe1a48
SaulLu <lucilesaul.com@gmail.com>
2022-03-10 11:25:48 +0000

    refacto

--- streamlit-app/code/app.py
+++ streamlit-app/code/app.py
@@ -11,6 +11,7 @@ from datasets import load_from_disk
 
 DATASET_DIR_PATH_BEFORE_CLEAN_SELECT = "/home/lucile/data"
 CLEANING_VERSIONS = ["clean_v2", "clean_v1"]
+OPERATION_TYPES =  ["Applied filter","Applied deduplication function","Applied map function"]
 
 
 def get_ds(ds_path):
@@ -33,105 +34,100 @@ def on_ds_change(ds_path):
     st.session_state["idx"] = 0
     st.session_state["ds_name"] = ds_path
 
+def get_log_stats_df(raw_log):
+    data = OrderedDict(
+        {
+            "Order": [],
+            "Name": [],
+            "Initial number of samples": [],
+            "Final number of samples": [],
+            "Initial size in bytes": [],
+            "Final size in bytes": [],
+        }
+    )
+
+    metric_dict = defaultdict(lambda: {})
+    order = 0
+    for line in raw_log.split("\n"):
+        for metric_name in list(data.keys()) + OPERATION_TYPES:
+
+            if metric_name == "Name" or metric_name == "Order":
+                continue
+
+            if metric_name not in line:
+                continue
+
+            if (
+                metric_name == "Removed percentage"
+                and "Removed percentage in bytes" in line
+            ):
+                continue
+
+            if (
+                metric_name == "Deduplicated percentage"
+                and "Deduplicated percentage in bytes" in line
+            ):
+                continue
+
+            value = line.split(metric_name)[1].split(" ")[1]
+
+            if metric_name in OPERATION_TYPES:
+                operation_name = value
+                metric_dict[operation_name]["Order"] = order
+                order += 1
+                continue
+
+            assert (
+                metric_name not in metric_dict[operation_name]
+            ), f"operation_name: {operation_name}\n\nvalue: {value}\n\nmetric_dict: {pp.pformat(metric_dict)} \n\nmetric_name: {metric_name} \n\nline: {line}"
+            metric_dict[operation_name][metric_name] = value
+    for name, data_dict in metric_dict.items():
+        for metric_name in data.keys():
+            if metric_name == "Name":
+                data[metric_name].append(name)
+                continue
+
+            data[metric_name].append(data_dict[metric_name])
+    df = pd.DataFrame(data)
+    df.rename(
+        {
+            "Initial size in bytes": "Initial size (GB)",
+            "Final size in bytes": "Final size (GB)",
+        },
+        axis=1,
+        inplace=True,
+    )
+    df["% samples removed"] = (
+        (
+            df["Initial number of samples"].astype(float)
+            - df["Final number of samples"].astype(float)
+        )
+        / df["Initial number of samples"].astype(float)
+        * 100
+    )
+    df["Size (GB) % removed"] = (
+        (
+            df["Initial size (GB)"].astype(float)
+            - df["Final size (GB)"].astype(float)
+        )
+        / df["Initial size (GB)"].astype(float)
+        * 100
+    )
+    return df
 
 def get_logs_stats(log_path):
+    with open(log_path) as f:
+        raw_log = f.read()
+
     try:
-        data = OrderedDict(
-            {
-                "Order": [],
-                "Name": [],
-                "Initial number of samples": [],
-                "Final number of samples": [],
-                "Initial size in bytes": [],
-                "Final size in bytes": [],
-            }
-        )
-        with open(log_path) as f:
-            content = f.read()
-        # subcontent = [line for line in content.split("\n") if "main" in line]
-        # st.write(subcontent)
-        metric_dict = defaultdict(lambda: {})
-        order = 0
-        for line in content.split("\n"):
-            for metric_name in list(data.keys()) + [
-                "Applied filter",
-                "Applied deduplication function",
-                "Applied map function",
-            ]:
-
-                if metric_name == "Name" or metric_name == "Order":
-                    continue
-
-                if metric_name not in line:
-                    continue
-
-                if (
-                    metric_name == "Removed percentage"
-                    and "Removed percentage in bytes" in line
-                ):
-                    continue
-
-                if (
-                    metric_name == "Deduplicated percentage"
-                    and "Deduplicated percentage in bytes" in line
-                ):
-                    continue
-
-                value = line.split(metric_name)[1].split(" ")[1]
-
-                if metric_name in [
-                    "Applied filter",
-                    "Applied deduplication function",
-                    "Applied map function",
-                ]:
-                    operation_name = value
-                    metric_dict[operation_name]["Order"] = order
-                    order += 1
-                    continue
-
-                assert (
-                    metric_name not in metric_dict[operation_name]
-                ), f"operation_name: {operation_name}\n\nvalue: {value}\n\nmetric_dict: {pp.pformat(metric_dict)} \n\nmetric_name: {metric_name} \n\nline: {line}"
-                metric_dict[operation_name][metric_name] = value
-        for name, data_dict in metric_dict.items():
-            for metric_name in data.keys():
-                if metric_name == "Name":
-                    data[metric_name].append(name)
-                    continue
-
-                data[metric_name].append(data_dict[metric_name])
-        df = pd.DataFrame(data)
-        df.rename(
-            {
-                "Initial size in bytes": "Initial size (GB)",
-                "Final size in bytes": "Final size (GB)",
-            },
-            axis=1,
-            inplace=True,
-        )
-        df["% samples removed"] = (
-            (
-                df["Initial number of samples"].astype(float)
-                - df["Final number of samples"].astype(float)
-            )
-            / df["Initial number of samples"].astype(float)
-            * 100
-        )
-        df["Size (GB) % removed"] = (
-            (
-                df["Initial size (GB)"].astype(float)
-                - df["Final size (GB)"].astype(float)
-            )
-            / df["Initial size (GB)"].astype(float)
-            * 100
-        )
+        df = get_log_stats_df(raw_log)
         st.dataframe(df)
     except Exception as e:
         st.write(e)
         st.write("Subset of the logs:")
         subcontent = [
             line
-            for line in content.split("\n")
+            for line in raw_log.split("\n")
             if "INFO - __main__" in line
             and "Examples of" not in line
             and "Examples n°" not in line
