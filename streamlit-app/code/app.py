33f74faa8a64a0512da48bee4e16e93b84764c91
SaulLu <lucilesaul.com@gmail.com>
2022-03-10 11:47:42 +0000

    refactor

--- streamlit-app/code/app.py
+++ streamlit-app/code/app.py
@@ -16,6 +16,7 @@ OPERATION_TYPES = [
     "Applied deduplication function",
     "Applied map function",
 ]
+MAX_LEN_DS_CHECKS = 1000
 
 
 def get_ds(ds_path):
@@ -23,19 +24,30 @@ def get_ds(ds_path):
     return ds
 
 
+def idx_plus(idx: int):
+    idx += 1
+    return idx % len(st.session_state["ds"])
+
+
+def idx_minus(idx: int):
+    idx -= 1
+    return idx % len(st.session_state["ds"])
+
+
 def on_click_next():
-    st.session_state["idx"] += 1
-    st.session_state["idx"] = st.session_state["idx"] % len(st.session_state["ds"])
+    st.session_state["idx_1"] = idx_plus(st.session_state["idx_1"])
+    st.session_state["idx_2"] = idx_plus(st.session_state["idx_2"])
 
 
 def on_click_previous():
-    st.session_state["idx"] -= 1
-    st.session_state["idx"] = st.session_state["idx"] % len(st.session_state["ds"])
+    st.session_state["idx_1"] = idx_minus(st.session_state["idx_1"])
+    st.session_state["idx_2"] = idx_minus(st.session_state["idx_2"])
 
 
 def on_ds_change(ds_path):
     st.session_state["ds"] = get_ds(ds_path)
-    st.session_state["idx"] = 0
+    st.session_state["idx_1"] = 0
+    st.session_state["idx_2"] = 1 if len(st.session_state["ds"]) > 1 else 0
     st.session_state["ds_name"] = ds_path
 
 
@@ -138,58 +150,18 @@ def get_logs_stats(log_path):
         st.write(subcontent)
 
 
-# Streamlit page
-st.set_page_config(page_title="Dataset explorer", layout="wide")
-st.write(
-    "The purpose of this application is to sequentially view the changes made to a dataset."
-)
-col_option_clean, col_option_ds = st.columns(2)
+def meta_component(idx_key: str = "idx_1"):
+    if "meta" not in st.session_state["ds"][st.session_state[idx_key]]:
+        return
 
-option_clean = col_option_clean.selectbox(
-    "Select the cleaning version",
-    CLEANING_VERSIONS,
-)
-DATASET_DIR_PATH = os.path.join(DATASET_DIR_PATH_BEFORE_CLEAN_SELECT, option_clean)
-
-dataset_names = sorted(list(os.listdir(DATASET_DIR_PATH)))
-with col_option_ds:
-    option_ds = st.selectbox(
-        "Select the dataset",
-        dataset_names,
-    )
-
-checks_path = os.path.join(DATASET_DIR_PATH, option_ds, "checks")
-checks_names = sorted(list(os.listdir(checks_path)))
-
-log_path = os.path.join(DATASET_DIR_PATH, option_ds, "logs.txt")
-get_logs_stats(log_path=log_path)
-
-option_check = st.selectbox(
-    "Select the operation applied to inspect",
-    checks_names,
-)
-ds_path = os.path.join(checks_path, option_check)
-
-# Initialization
-if "ds" not in st.session_state:
-    st.session_state["ds"] = get_ds(ds_path)
-    st.session_state["ds_name"] = ds_path
-    st.session_state["idx"] = 0
-
-if ds_path != st.session_state["ds_name"]:
-    on_ds_change(ds_path)
-
-if len(st.session_state["ds"]) == 1000:
-    st.warning(
-        "Attention only a subset of size 1000 of the modified / filtered examples can be shown in this application"
-    )
-with st.expander("See details of the available checks"):
-    st.write(st.session_state["ds"])
+    with st.expander("See meta field of the example"):
+        meta = st.session_state["ds"][st.session_state["idx_1"]]["meta"]
+        st.write(meta)
 
 
-if "_filter_" in option_check:
-    idx_1 = st.session_state["idx"]
-    idx_2 = (st.session_state["idx"] + 1) % len(st.session_state["ds"])
+def filter_page():
+    idx_1 = st.session_state["idx_1"]
+    idx_2 = st.session_state["idx_2"]
     text_1 = st.session_state["ds"][idx_1]["text"]
     text_2 = st.session_state["ds"][idx_2]["text"]
 
@@ -218,26 +190,21 @@ if "_filter_" in option_check:
     col_1, col_2 = st.columns(2)
     with col_1:
         st.subheader(f"Example n°{idx_1}")
-        if "meta" in st.session_state["ds"][st.session_state["idx"]]:
-            with st.expander("See meta field of the example"):
-                meta = st.session_state["ds"][st.session_state["idx"]]["meta"]
-                st.write(meta)
+        meta_component(idx_key="idx_1")
         text_1_show = text_1.replace("\n", "<br>")
         st.markdown(f"<div>{text_1_show}</div>", unsafe_allow_html=True)
 
     with col_2:
         st.subheader(f"Example n°{idx_2}")
-        if "meta" in st.session_state["ds"][st.session_state["idx"]]:
-            with st.expander("See meta field of the example"):
-                meta = st.session_state["ds"][st.session_state["idx"] + 1]["meta"]
-                st.write(meta)
+        meta_component(idx_key="idx_2")
         text_2_show = text_2.replace("\n", "<br>")
         st.markdown(f"<div>{text_2_show}</div>", unsafe_allow_html=True)
 
-else:
+
+def dedup_or_cleaning_page():
     col_button_previous, col_title, col_button_next = st.columns(3)
     col_title.markdown(
-        f"<h1 style='text-align: center'>Example n°{st.session_state['idx']}</h1>",
+        f"<h1 style='text-align: center'>Example n°{st.session_state['idx_1']}</h1>",
         unsafe_allow_html=True,
     )
     col_button_next.button(
@@ -257,8 +224,8 @@ else:
         kwargs=None,
     )
 
-    text = st.session_state["ds"][st.session_state["idx"]]["text"]
-    old_text = st.session_state["ds"][st.session_state["idx"]]["old_text"]
+    text = st.session_state["ds"][st.session_state["idx_1"]]["text"]
+    old_text = st.session_state["ds"][st.session_state["idx_1"]]["old_text"]
     st.markdown(
         f"<h2 style='text-align: center'>Changes applied</h1>", unsafe_allow_html=True
     )
@@ -268,18 +235,54 @@ else:
     with col_text_2:
         st.subheader("New text")
     diff_viewer.diff_viewer(old_text=old_text, new_text=text, lang="none")
-    if "meta" in st.session_state["ds"][st.session_state["idx"]]:
-        with st.expander("See meta field of the example"):
-            meta = st.session_state["ds"][st.session_state["idx"]]["meta"]
-            st.write(meta)
+    meta_component(idx_key="idx_1")
 
     with st.expander("See full old and new texts of the example"):
+        text_show = text.replace("\n", "<br>")
+        old_text_show = old_text.replace("\n", "<br>")
+
         col_1, col_2 = st.columns(2)
         with col_1:
             st.subheader("Old text")
-            old_text_show = old_text.replace("\n", "<br>")
             st.markdown(f"<div>{old_text_show}</div>", unsafe_allow_html=True)
         with col_2:
             st.subheader("New text")
-            text_show = text.replace("\n", "<br>")
             st.markdown(f"<div>{text_show}</div>", unsafe_allow_html=True)
+
+
+# Streamlit page
+st.set_page_config(page_title="Dataset explorer", layout="wide")
+st.write(
+    "The purpose of this application is to sequentially view the changes made to a dataset."
+)
+col_option_clean, col_option_ds = st.columns(2)
+
+option_clean = col_option_clean.selectbox(
+    "Select the cleaning version", CLEANING_VERSIONS
+)
+
+DATASET_DIR_PATH = os.path.join(DATASET_DIR_PATH_BEFORE_CLEAN_SELECT, option_clean)
+dataset_names = sorted(list(os.listdir(DATASET_DIR_PATH)))
+option_ds = col_option_ds.selectbox("Select the dataset", dataset_names)
+
+checks_path = os.path.join(DATASET_DIR_PATH, option_ds, "checks")
+checks_names = sorted(list(os.listdir(checks_path)))
+
+log_path = os.path.join(DATASET_DIR_PATH, option_ds, "logs.txt")
+get_logs_stats(log_path=log_path)
+
+option_check = st.selectbox("Select the operation applied to inspect", checks_names)
+ds_path = os.path.join(checks_path, option_check)
+
+if "ds" not in st.session_state or ds_path != st.session_state["ds_name"]:
+    on_ds_change(ds_path)
+
+if len(st.session_state["ds"]) == MAX_LEN_DS_CHECKS:
+    st.warning(
+        f"Note: only a subset of size {MAX_LEN_DS_CHECKS} of the modified / filtered examples can be shown in this application"
+    )
+with st.expander("See details of the available checks"):
+    st.write(st.session_state["ds"])
+
+
+_ = filter_page() if "_filter_" in option_check else dedup_or_cleaning_page()
