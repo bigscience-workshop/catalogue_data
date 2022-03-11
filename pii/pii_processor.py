069681b8f351dda25922111d1b22354b93cd61fa
SaulLu <lucilesaul.com@gmail.com>
2022-03-11 10:30:01 +0000

    refacto

--- pii/pii_processor.py
+++ pii/pii_processor.py
@@ -11,11 +11,13 @@ logger = logging.getLogger(__name__)
 
 def get_args():
     parser = argparse.ArgumentParser(description='Load a dataset.')
-    parser.add_argument('--load_from_disk', action="store_true")
-    parser.add_argument('--dataset_path', type=Path)
-    parser.add_argument('--save_path', type=Path)
+    parser.add_argument('--load-from-disk', action="store_true")
+    parser.add_argument('--save-to-json', action="store_true")
+    parser.add_argument('--dataset-path', type=Path)
+    parser.add_argument('--save-path', type=Path)
     parser.add_argument("--num-proc", type=int, default=1)
     parser.add_argument("--batch-size", type=int)
+    parser.add_argument("--save-batch-size", type=int)
     args = parser.parse_args()
     return args
 
@@ -28,7 +30,7 @@ def run_pii(exs, lang):
     only "default"/non-language-specific regexes.
     """
     regex_metadata = []
-    org_text = []
+    old_text = []
     new_text = []
     modified = []
     for text in exs["text"]:
@@ -39,18 +41,18 @@ def run_pii(exs, lang):
             # !!! REDACTION HAPPENS HERE !!!
             redacted_str, metadata = redact_pii(text, matches)
             regex_metadata.append(metadata)
-            org_text.append(text)
+            old_text.append(text)
             new_text.append(redacted_str)
             modified.append(True)
         else:
             regex_metadata.append([])
-            org_text.append(text)
+            old_text.append(text)
             new_text.append(text)
             modified.append(False)
     return {
         "regex_metadata": regex_metadata,
-        "org_text": org_text,
-        "new_text": new_text,
+        "old_text": old_text,
+        "text": new_text,
         "modified": modified
     }
 
@@ -78,19 +80,47 @@ if __name__ == '__main__':
         batch_size=args.batch_size,
         num_proc=args.num_proc
     )
+
+    ds_final = ds.remove_columns([
+        "regex_metadata",
+        "old_text",
+        "modified"
+    ])
     
-    logger.info(f" ===== Saving dataset =====")
+    logger.info(f" ===== Saving Final dataset =====")
     logger.info(f"Saving to final dataset at {args.save_path}.")
     tmp_save_path = Path(args.save_path.parent, f"tmp-{args.save_path.name}")
-    if len(ds) == 0:
+    if len(ds_final) == 0:
         logger.info("Dataset was empty. Not saving anything.")
         return
     if args.save_to_json:
-        ds.to_json(
+        ds_final.to_json(
             tmp_save_path,
             num_proc=args.num_proc
+            batch_size=args.save_batch_size
         )
     else:
-        ds.save_to_disk(tmp_save_path)
+        ds_final.save_to_disk(tmp_save_path)
     tmp_save_path.rename(args.save_path)
-    logger.info(f" ===== Finish successfully =====")
+    logger.info(f" ===== Final dataset saved successfully =====")
+
+    idx_samples = random.sample(range(len(ds)), min(len(ds), args.sampling_size))
+    ds_checks = ds.select(idx_samples)
+    ds_checks = ds_checks.rename_column("text", "old_text")
+
+    logger.info(f" ===== Saving check dataset =====")
+    logger.info(f"Saving check dataset at {args.save_check_path}.")
+    tmp_save_path = Path(args.save_check_path.parent, f"tmp-{args.save_check_path.name}")
+    if len(ds_checks) == 0:
+        logger.info("Dataset was empty. Not saving anything.")
+        return
+    if args.save_check_to_json:
+        ds_checks.to_json(
+            tmp_save_path,
+            num_proc=args.num_proc
+            batch_size=args.save_batch_size
+        )
+    else:
+        ds_checks.save_to_disk(tmp_save_path)
+    tmp_save_path.rename(args.save_check_path)
+    logger.info(f" ===== Check dataset saved successfully =====")
